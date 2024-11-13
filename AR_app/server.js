b0d6f966f4f01fc2421ce296320972d672ee632e
ksw2370040 <ksw2370040@stu.o-hora.ac.jp>
2024-11-13 11:25:23 +0900

    login

--- AR_app/server.js
+++ AR_app/server.js
@@ -3,12 +3,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const markerRoutes = require('./routes/markerRoutes'); // markerRoutesをインポート
+const adminlogin = require('./routes/admin'); // markerRoutesをインポート
+
+// ボディパーサー設定 (POSTデータを受け取るため)
+app.use(express.urlencoded({ extended: true }));  // URLエンコードされたデータの処理
+app.use(express.json());  // JSONデータの処理
 
 // public フォルダを静的ファイルの提供場所として指定
 app.use(express.static(path.join(__dirname, 'public')));  // 'public' フォルダを静的ファイルとして公開
 
 // データベースに関するルートを設定
 app.use('/api', markerRoutes); // markerRoutes を /api パスにマウント
+
+app.use('/api', adminlogin);  // /api/login エンドポイントが有効になります
 
 // サーバーの起動
 const PORT = 8080;
