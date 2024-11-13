8ac277bd79e2603cc47d5e45d9fd6cbaec62b244
ksw2370040 <ksw2370040@stu.o-hora.ac.jp>
2024-11-13 10:56:46 +0900

    new

---
+++ AR_app/routes/newAdmin.js
@@ -0,0 +1,23 @@
+// routes/adminRoutes.js
+const express = require('express');
+const router = express.Router();
+const connection = require('../config');
+
+// ログインエンドポイント
+router.post('/newadmin', (req, res) => {
+    const { adminname, password } = req.body;
+    const query = 'SELECT * FROM ADMIN WHERE name = $1 AND password = $2;';
+    connection.query(query, [adminname, password], (err, results) => {
+        if (err) {
+            console.error('データベースエラー:', err);
+            return res.status(500).send('サーバーエラー');
+        }
+        if (results.rows.length > 0) {
+            res.redirect(`/AR_admin/menu.html?adminname=${encodeURIComponent(adminname)}`);
+        } else {
+            res.status(401).send('ユーザー名またはパスワードが間違っています');
+        }
+    });
+});
+
+module.exports = router;
