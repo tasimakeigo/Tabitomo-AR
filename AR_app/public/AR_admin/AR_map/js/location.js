b0b293c20662f3efe9d1dbb9e1d6ca1142685b08
ksw2370040 <ksw2370040@stu.o-hora.ac.jp>
2025-01-10 11:10:01 +0900

    aaaa

---
+++ AR_app/public/AR_admin/AR_map/js/location.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // モデル情報を取得するためのAPI呼び出し
+    fetch('/location')  // モデル情報を取得するエンドポイント
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('ネットワークエラー: ' + response.status);
+            }
+            return response.json();  // JSONレスポンスを取得
+        })
+        .then(data => {
+            const location = document.querySelector('.location ul');  // モデル情報を表示する要素を選択
+
+            // 取得したモデル情報をリストに表示
+            data.forEach(model => {
+                const listItem = document.createElement('li');
+
+                // モデル情報をリスト項目に挿入
+                listItem.textContent = ` ${model.locationname} `;
+                listItem.innerHTML = `
+                <a href="map.html?address=${model.address}&locationname=${model.locationname}" class="locationid">
+                    ${model.locationname}
+                </a>
+            `;
+
+                location.appendChild(listItem); // リストに追加
+            });
+        })
+        .catch(error => {
+            console.error('モデル情報の取得中にエラーが発生しました:', error);
+            alert('モデル情報の取得に失敗しました。');
+        });
+});
+
